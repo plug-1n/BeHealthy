18bdd9d452a3af6ed69bf21e1aad6bc3f9eb48fd
Svesh1-800 <60923071+Svesh1-800@users.noreply.github.com>
2022-01-24 10:24:51 +0300

    Tempates are being displayed with static files

--- generator/views.py
+++ generator/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def test(self):
-    return HttpResponse("HELLO")
+def test(request):
+    return render(request,  'generator/index.html')
 # Create your views here.
