3f6b263184068b23256f755dfec3e0b6dfc0d419
Ed Khugaev <ed.khugaev@gmail.com>
2023-03-22 16:39:55 +0300

    update requirements

--- main.py
+++ main.py
@@ -5,10 +5,22 @@ from  fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from sqladmin import Admin, ModelView
+from models import *
 
 app = FastAPI()
+admin = Admin(app, engine)
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+class UserAdmin(ModelView, model=User):
+    name = "Челик"
+    name_plural = "Челики"
+    icon = "fa-solid fa-user"
+    
+    column_list = [User.id, User.name]
+
+
+admin.add_view(UserAdmin)
 
 app.mount("/static", StaticFiles(directory=ROOT_DIR), name="static")
 templates = Jinja2Templates(directory=ROOT_DIR)
